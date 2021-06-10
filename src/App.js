6657f611e395034ca3c685b34d90fef3204bbd58
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-06-10 00:23:27 -0300

    iniciando componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddTutorial from "./components/AddFilme";
+import Tutorial from "./components/Filme";
+import TutorialsList from "./components/FilmesList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/filmes" className="navbar-brand">
+          bezKoder
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/filmes"} className="nav-link">
+              Filmes
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/filmes"]} component={FilmesList} />
+          <Route exact path="/add" component={AddFilme} />
+          <Route path="/filmes/:id" component={Filme} />
+        </Switch>
+      </div>
     </div>
   );
 }
